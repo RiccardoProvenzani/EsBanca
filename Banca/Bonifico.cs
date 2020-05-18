fb73e4075700607e26b23c60265c2189f44bd8ff
RiccardoProvenzani <riccardopia.p@gmail.com>
2020-05-18 09:31:36 +0200

    Primo commit

---
+++ Banca/Bonifico.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banca
+{
+    public class Bonifico : Movimento
+    {
+        private string mandante { get; } // chi manda
+		private string ricevente { get; } // chi riceve
+		private double importo { get; } // importo della transazione
+		private bool online { get; } = false; // indica se il bonifico è stato fatto da un conto online
+
+        /// <summary>
+        /// costruttore
+        /// </summary>
+        /// <param name="data_"></param>
+        /// <param name="mandante_"></param>
+        /// <param name="ricevente_"></param>
+        /// <param name="importo_"></param>
+        public Bonifico(DateTime data_, string mandante_, string ricevente_, double importo_) :
+            base(data_)
+        {
+            data = data_;
+            mandante = mandante_;
+            ricevente = ricevente_;
+            importo = importo_;
+        }
+
+        /// <summary>
+        /// imposta la variabile online a true per indicare che è il bonifico è stato fatto da un conto online
+        /// </summary>
+        public void SetOnline()
+        {
+            online = true;
+        }
+    }
+}
---
+++ Banca/Intestatario.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Banca
+{
+    public class Intestatario
+    {
+        private string nomeCompleto { get; } // nome dell'intestatario
+		private DateTime dataNascita { get; } // data nascita dell'intestatario
+		private string indirizzo { get; } // indirizzo dell'intestatario 
+		private string email { get; } // e-mail dell'intestatario
+
+		/// <summary>
+		/// costruttore
+		/// </summary>
+		/// <param name="nomeCompleto_"></param>
+		/// <param name="dataNascita_"></param>
+		/// <param name="indirizzo_"></param>
+		/// <param name="email_"></param>
+		public Intestatario(string nomeCompleto_, DateTime dataNascita_, string indirizzo_, string email_)
+        {
+            nomeCompleto = nomeCompleto_;
+            dataNascita = dataNascita_;
+            indirizzo = indirizzo_;
+            email = email_;
+        }
+    }
+}
